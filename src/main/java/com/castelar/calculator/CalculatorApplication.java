c8655344ab35b098d8e14b8283dd020232aa52b5
miguelcaste <secreto747@hotmail.com>
2022-04-18 21:59:19 +0200

    transpassing logic to ui

--- src/main/java/com/castelar/calculator/CalculatorApplication.java
+++ src/main/java/com/castelar/calculator/CalculatorApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class CalculatorApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(CalculatorApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 233, 326);
         stage.setTitle("Calculadora");
         stage.setScene(scene);
--- src/main/java/com/castelar/calculator/CalculatorController.java
+++ src/main/java/com/castelar/calculator/CalculatorController.java
@@ -2,6 +2,7 @@ package com.castelar.calculator;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class CalculatorController {
@@ -9,10 +10,135 @@ public class CalculatorController {
     String bufferString="";
     boolean operacion=false;
 
+    private static String button_string;
 
+    private static String cadena_entrada="";// getText
+    private static String cadena_salida = "0"; // setText
+
+    private static boolean primera = true; // Control: Primer operando mo inicio
+    // Controlar posibles entradas erróneas por ejemplo + + = =
+    private static boolean bandera_operacion = false; // Control: Se esta realizando una operación
+    private static String operador = ""; // Operador
+
+    private static double valor_primer_operando = 0.0;
+    private static double valor_segundo_operando = 0.0;
+
+    private static double resultado = 0.0;
 
     @FXML
     TextField txtField;
+
+    // RESET
+    public static void casoAC(){
+        System.out.println("Caso AC");
+
+        cadena_salida = "0";
+        primera=true;
+    }
+
+    // Si pulso 0 y hay un 0
+    public static void caso0(){
+        System.out.println("Caso 0");
+
+        if (!primera){ //Si no es el primero
+            cadena_salida = cadena_salida + cadena_entrada;
+        }
+        else {
+            cadena_salida = "0";
+        }
+    }
+
+    // Si pulso n y hay n, desplazo a la izquierda
+    public static void casoNumeroPunto(){
+        System.out.println("Caso Numero o Punto");
+
+        cadena_salida = cadena_salida + cadena_entrada;
+        primera=false;
+    }
+
+    // si pulso operador me voy quedando con el último, si
+    public static void casoOperador(){
+        System.out.println("Caso Operador");
+
+
+        operador = cadena_entrada;
+        valor_primer_operando = Double.parseDouble(cadena_salida);
+        bandera_operacion = true;
+        primera=true;
+    }
+
+    // obtengo el resultado siempre que haya operando
+    // si hay segundo operando actuaría como un igual controlar esta situación.
+    public static void casoIgual(){
+        //Falta comprobación
+
+        System.out.println("Caso Igual");
+
+
+        valor_segundo_operando =Double.parseDouble(cadena_salida);
+
+        switch(operador)
+        {
+            case "+" :
+                resultado = valor_primer_operando + valor_segundo_operando;
+                break;
+            case "-" :
+                resultado = valor_primer_operando - valor_segundo_operando;
+                break;
+            case "*" :
+                resultado = valor_primer_operando * valor_segundo_operando;
+                break;
+            case "/" :
+                resultado = valor_primer_operando / valor_segundo_operando;
+                break;
+        }
+
+        //Ver si es entero
+
+        if((resultado-(int)resultado)==0){
+            cadena_salida=(int)resultado+"";
+        }
+        else {
+            cadena_salida=resultado+"";
+        }
+
+        primera=true;
+    }
+
+
+
+
+    @FXML
+    private void onEveryButtonClick(ActionEvent actionEvent) {
+        Button button = (Button) actionEvent.getSource();
+        button_string = button.getId().substring(3);
+        //System.out.println(button.getId().substring(3));
+        //System.out.println(primera);
+        if(primera){
+            cadena_salida="";
+        }
+        switch (button_string){
+            case "AC": //reset
+                casoAC();
+                break;
+            case "0":
+                caso0();
+                break;
+            case "1","2","3","4","5","6","7","8","9","Coma":
+                casoNumeroPunto();
+                break;
+            case "Add","Sub","Mul","Div","Perc": // operador
+                //caso que debería contemplar 2 + 2 + 2 // El más actuaría como un igual // usar bandera
+                casoOperador();
+                break;
+            case "=": //igual //comprobar que hay operación
+                casoIgual();
+        }
+    }
+
+
+
+
 
 
     @FXML
